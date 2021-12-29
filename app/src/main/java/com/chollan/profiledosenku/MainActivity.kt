01abfd967a922df30ba07fded5d22a8aa043721f
Mac Chollan <kholan.childs404@gmail.com>
2021-12-29 21:05:15 +0700

    Add action bar

--- app/src/main/java/com/chollan/profiledosenku/MainActivity.kt
+++ app/src/main/java/com/chollan/profiledosenku/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.chollan.profiledosenku
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getSupportActionBar()!!.elevation = 0F
+
         var rvDosen : RecyclerView = findViewById(R.id.rv_dosen)
         rvDosen.setHasFixedSize(true)
 
@@ -23,5 +31,39 @@ class MainActivity : AppCompatActivity() {
         rvDosen.adapter = cardViewHolder
 
         rvDosen.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                val i = Intent(this, ProfileActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
     }
 }
